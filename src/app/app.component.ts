4898fe90bb8734c63977d74d40b93d01c3f231e5
Daniel Figueira <danfig87@gmail.com>
2021-07-22 18:15:23 +0100

    Finished share screen for mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'article-preview-component';
+  isShareVisible = false;
+
+  toggleShare(): void {
+    this.isShareVisible = !this.isShareVisible;
+  }
 }
